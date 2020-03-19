4ae51786df5e13c7a226aa54ed5675cb334144bd
Nattguld <6783744+randqm@users.noreply.github.com>
2020-03-19 20:04:43 +0100

    organizing the framework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,17 @@ import { UsersComponent } from './users/user/users.component';
 import { NourishmentComponent } from './nutrition/nourishment/nourishment.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserComponent } from './users/user/user.component';
+import { RegisterComponent } from './users/register/register.component';
+import { AuthenticateComponent } from './users/authenticate/authenticate.component';
+import { LoginComponent } from './users/authenticate/login/login.component';
+import { DetailsComponent } from './users/account/details/details.component';
+import { SettingsComponent } from './users/account/settings/settings.component';
+import { UpgradeComponent } from './users/account/upgrade/upgrade.component';
+import { OverviewComponent } from './admin/users/overview/overview.component';
+import { SearchComponent } from './admin/users/search/search.component';
+import { CreateComponent } from './admin/users/crud/create/create.component';
+import { SuggestComponent } from './nutrition/nourishments/suggest/suggest.component';
+import { SubmitComponent } from './nutrition/nourishments/submit/submit.component';
 
 @NgModule({
   declarations: [
@@ -32,7 +43,18 @@ import { UserComponent } from './users/user/user.component';
     HeroSearchComponent,
     NourishmentComponent,
     UsersComponent,
-    UserComponent
+    UserComponent,
+    RegisterComponent,
+    AuthenticateComponent,
+    LoginComponent,
+    DetailsComponent,
+    SettingsComponent,
+    UpgradeComponent,
+    OverviewComponent,
+    SearchComponent,
+    CreateComponent,
+    SuggestComponent,
+    SubmitComponent
   ],
   imports: [
     HttpClientModule,
